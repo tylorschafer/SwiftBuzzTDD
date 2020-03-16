c30914e70af091191c315e347ac21091a984fb40
Tylor Schafer <tylorschafer@gmail.com>
2020-03-15 21:49:27 -0600

    Setup Brain Instantiation to DRY tests

--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -2,34 +2,31 @@ import XCTest
 @testable import FizzBuzz
 
 class BrainTests: XCTestCase {
+    
+    let brain = Brain()
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
     func testIsDivisibleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 3)
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisbleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 2)
         XCTAssertEqual(result, false)
     }
     
     func testIsDivisibleByFive() {
-        let brain = Brain()
         let result = brain.isDivisibleByFive(number: 5)
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisibleByFive() {
-        let brain = Brain()
         let result = brain.isDivisibleByFive(number: 4)
         XCTAssertEqual(result, false)
     }
