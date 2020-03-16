ab30e92b6c06c38e5702d0d6d5f08154e2e3a2fd
Tylor Schafer <tylorschafer@gmail.com>
2020-03-15 20:29:46 -0600

    Delete default UITests and add replace unit tests file

---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,21 @@
+//
+//  BrainTests.swift
+//  FizzBuzzTests
+//
+//  Created by Tylor Schafer on 3/15/20.
+//  Copyright © 2020 Tylor Schafer. All rights reserved.
+//
+
+import XCTest
+
+class BrainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+}
