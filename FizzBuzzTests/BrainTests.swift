c731d89995ba0b2f8ea2557d29e4036fa966ee2f
Tylor Schafer <tylorschafer@gmail.com>
2020-03-15 20:51:59 -0600

    Add isDivisibleByThree test

--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -17,5 +17,9 @@ class BrainTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
 }
